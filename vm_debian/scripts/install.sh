1c2ab81deac99a43bed504c1955ffa4646b6c2dd
KpihX <kapoivha@gmail.com>
2026-05-08 08:00:42 +0200

    debug: removing passwords for direct serial access

--- vm_debian/scripts/install.sh
+++ vm_debian/scripts/install.sh
@@ -202,13 +202,15 @@ package_update: true
 packages:
   - qemu-guest-agent
 runcmd:
+  - "passwd -d root"
+  - "passwd -d kpihx"
+  - "ip addr > /root/network_debug.txt"
+  - "ip link show >> /root/network_debug.txt"
   - "ip addr add 10.10.10.101/24 dev eth0 || true"
   - "ip addr add 10.10.10.101/24 dev ens18 || true"
   - "ip link set eth0 up || true"
   - "ip link set ens18 up || true"
   - "ip route add default via 10.10.10.1 || true"
-  - "echo 'kpihx:pass123' | chpasswd"
-  - "echo 'root:pass123' | chpasswd"
   - rm -f /etc/resolv.conf
   - printf "nameserver ${DNS%%,*}\n" > /etc/resolv.conf
   - "systemctl stop systemd-resolved || true"
